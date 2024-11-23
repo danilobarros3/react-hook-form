5f14b0e11bf1975f6a129c66ed0270d93806e717
danilobarros3 <danilo.lionsoft@gmail.com>
2024-11-23 09:57:08 -0300

    :rocket: deploy

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import type { NextPage } from "next";
 import { useForm, FormProvider } from "react-hook-form";
 import * as yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
-import { RHFTextInput } from "./components/RHFTextInput";
+import { RHFTextInput } from "../components/RHFTextInput";
 
 interface ILoginForm {
   email: string;
